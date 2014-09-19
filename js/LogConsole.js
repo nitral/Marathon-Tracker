f551702dd4ff2ad8d263e095f4e358a07846b8fe
Nilay Binjola <nilaybinjola@gmail.com>
2014-09-20 00:38:29 +0530

    Added: Log Console Module.

--- js/LogConsole.js
+++ js/LogConsole.js
@@ -2,8 +2,26 @@
 
 // Object Instantiation.
 	// Pass Console DOM Object.
-// Other object Decalarations.
-	
+function LogConsole(terminal) {
+	this.terminal = terminal;
+	this.length = 0;
+}
+
+// Other object Declarations.
+
 // Log Console Object Methods Definition.
+LogConsole.prototype.append = function(message) {
+	this.terminal.innerHTML = this.terminal.innerHTML + message + "<br />";
+	this.length++;
+}
+
+LogConsole.prototype.clear = function() {
+	this.terminal.innerHTML = "";
+	this.length = 0;
+}
 
 // Log Console Objections Methods - Special Alerts.
+LogConsole.prototype.specialAlert = function(message) {
+	this.append(message);
+	alert(message);
+}
